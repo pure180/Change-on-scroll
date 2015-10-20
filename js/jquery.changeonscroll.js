77325d576e00ddcb9c43d0d8a27d07a9ce32f153
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-10-20 09:37:56 +0200

    gh pages from new settings

--- js/jquery.changeonscroll.js
+++ js/jquery.changeonscroll.js
@@ -11,26 +11,26 @@
   'use strict';
 
   var ChangeOnScroll = function(element, options) {
-    this.$element       = $(element);
-    this.data           = this.$element.data();
-    this.options        = $.extend({}, ChangeOnScroll.DEFAULTS, this.data, options);
+    this.$element       = $(element)
+    this.data           = this.$element.data()
+    this.options        = $.extend({}, ChangeOnScroll.DEFAULTS, this.data, options)
 
-    this.pos            = $(window).scrollTop();
-    this.top            = this.offsetTop();
-    this.bottom         = this.offsetBottom();
+    this.pos            = $(window).scrollTop()
+    this.top            = this.offsetTop()
+    this.bottom         = this.offsetBottom()
 
-    this.start          = String(this.options.start).indexOf(',') > -1 ? this.options.start.split(",") : this.options.start;
-    this.end            = String(this.options.end).indexOf(',') > -1 ? this.options.end.split(",") : this.options.end;
-    this.style          = String(this.options.style).indexOf(',') > -1 ? this.options.style.split(",") : this.options.style;
+    this.start          = String(this.options.start).indexOf(',') > -1 ? this.options.start.split(",") : this.options.start
+    this.end            = String(this.options.end).indexOf(',') > -1 ? this.options.end.split(",") : this.options.end
+    this.style          = String(this.options.style).indexOf(',') > -1 ? this.options.style.split(",") : this.options.style
 
-    this.unit           = String(this.options.unit).indexOf(',') > -1 && this.options.unit ? this.options.unit.split(",") :  String(this.options.unit);
+    this.unit           = String(this.options.unit).indexOf(',') > -1 && this.options.unit ? this.options.unit.split(",") :  String(this.options.unit)
 
     //$('.inidcator.top').css('top', this.top )
     //$('.inidcator.bottom').css('top', this.bottom )
 
-    this.Init();
-  };
-  ChangeOnScroll.VERSION  = '2.0.0';
+    this.Init()
+  }
+  ChangeOnScroll.VERSION  = '2.0.0'
 
   ChangeOnScroll.DEFAULTS = {
     beforeClass       : 'scroller_before',
@@ -42,144 +42,149 @@
     start             : '',
     end               : '',
     factor            : 1
-  };
+  }
 
   ChangeOnScroll.prototype.Init = function() {
-    var scrolled = null;
+    var scrolled = null
 
     if(this.pos >= this.top && this.pos <= this.bottom ) {
-      scrolled = 'inside';
-      this.Inside();
+      scrolled = 'inside'
+      this.Inside()
     } else if ( this.pos >= this.bottom ) {
-      scrolled = 'after';
-      this.After();
+      scrolled = 'after'
+      this.After()
     } else if ( this.pos <= this.top ) {
-      scrolled = 'before';
-      this.Before();
+      scrolled = 'before'
+      this.Before()
     } else {
       //console.log('else: ' + scroll_pos)
     }
 
-    var eventType = scrolled;
-    var e = $.Event(eventType + '.changeonscroll');
-    this.$element.trigger(e);
+    var eventType = scrolled
+    var e = $.Event(eventType + '.changeonscroll')
+    this.$element.trigger(e)
 
-    if (e.isDefaultPrevented()) return;
-  };
+    if (e.isDefaultPrevented()) return
+  }
 
   ChangeOnScroll.prototype.Inside = function() {
     this.$element.one()
       .addClass(this.options.whileClass)
       .removeClass(this.options.beforeClass)
-      .removeClass(this.options.afterClass);
-    this.CalculatePositionAndSetStyles();
-  };
+      .removeClass(this.options.afterClass)
+    this.CalculatePositionAndSetStyles()
+  }
 
   ChangeOnScroll.prototype.Before = function() {
     this.$element.one()
       .removeClass(this.options.whileClass)
-      .addClass(this.options.beforeClass);
-    this.SetStyles(this.start);
-  };
+      .addClass(this.options.beforeClass)
+    this.SetStyles(this.start)
+  }
 
   ChangeOnScroll.prototype.After = function() {
     this.$element.one()
       .removeClass(this.options.whileClass)
-      .addClass(this.options.afterClass);
-    this.SetStyles(this.end);
-  };
+      .addClass(this.options.afterClass)
+    this.SetStyles(this.end)
+  }
 
   ChangeOnScroll.prototype.offsetTop = function() {
-    var trigger = this.options.top;
-    var offsetTop;
+    var trigger = this.options.top
+    var offsetTop
     if(trigger){
-      offsetTop = typeof trigger === 'number' ? trigger :  $(trigger).offset().top;
+      offsetTop = typeof trigger === 'number' ? trigger :  $(trigger).offset().top
     } else {
-      offsetTop = typeof this.options.top === 'number' ? this.options.top : this.$element.offset().top;
+      offsetTop = typeof this.options.top === 'number' ? this.options.top : this.$element.offset().top
     }
     return offsetTop;
-  };
+  }
 
   ChangeOnScroll.prototype.offsetBottom = function() {
-    var trigger = this.options.bottom;
-    var offsetBottom;
+    var trigger = this.options.bottom
+    var offsetBottom
     if(trigger){
-      offsetBottom = typeof trigger === 'number' ? trigger : $(trigger).offset().top;
+      offsetBottom = typeof trigger === 'number' ? trigger : $(trigger).offset().top
     } else {
-      offsetBottom = typeof this.options.bottom === 'number' ? this.options.bottom : this.$element.offset().top;
+      offsetBottom = typeof this.options.bottom === 'number' ? this.options.bottom : this.$element.offset().top
     }
     return offsetBottom;
-  };
+  }
 
   ChangeOnScroll.prototype.Percentage = function() {
-    var result = (this.pos - this.top) / (this.bottom - this.top);
-    return result.toFixed(6);
-  };
+    var result = (this.pos - this.top) / (this.bottom - this.top)
+    return result.toFixed(6)
+  }
 
   ChangeOnScroll.prototype.Calculate = function(index) {
-    var start = typeof this.start === 'number' ? this.start : this.start[index];
-    var end = typeof this.end === 'number' ? this.end : this.end[index];
-    var forward = Number(start) + ( this.Percentage() * (end - start) * this.options.factor);
-    var backward = Number(start) - ( this.Percentage() * ( start - end) * this.options.factor);
-    var result = this.options.reverse ? backward : forward;
-    return result;
-  };
+    var start = typeof this.start === 'number' ? this.start : this.start[index]
+    var end = typeof this.end === 'number' ? this.end : this.end[index]
+    var forward = Number(start) + ( this.Percentage() * (end - start) * this.options.factor )
+    var backward = Number(start) - ( this.Percentage() * ( start - end) * this.options.factor )
+    var result = this.options.reverse ? backward : forward
+    return result
+  }
 
   ChangeOnScroll.prototype.SetStyles = function(value) {
-    var style = '', separator = '';
+    var style = '', separator = ''
     if(this.style && typeof this.style === 'string') {
-      this.$element.css(this.style, value * this.options.factor );
+      this.$element.css(this.style, value * this.options.factor )
     } else {
       for(var i in this.style){
-        separator = this.style.length - 1 === i ? '' : ', ';
-        style += this.style[i] + ':' + value[i] * this.options.factor + separator;
+        separator = this.style.length - 1 === i ? '' : ', '
+        style += this.style[i] + ':' + value[i] * this.options.factor + separator
       }
-      var styles = eval('({' + style + '})');
-      this.$element.css( styles );
+      var styles = eval('({' + style + '})')
+      this.$element.css( styles )
     }
-  };
+  }
 
   ChangeOnScroll.prototype.CalculatePositionAndSetStyles = function() {
-    var style = '', separator = '';
+    var style = '', separator = ''
     if(this.style && typeof this.style === 'string') {
-      this.$element.css( this.style, this.Calculate(0));
+      this.$element.css( this.style, this.Calculate(0))
     } else {
       for(var i in this.style) {
-        separator = this.style.length - 1 === i ? ' ' : ', ';
-        style += this.style[i] + ':' + this.Calculate(i) + separator;
+        separator = this.style.length - 1 === i ? ' ' : ', '
+        style += this.style[i] + ':' + this.Calculate(i) + separator
       }
-      var styles = eval('({' + style + '})');
-      this.$element.css( styles );
+      var styles = eval('({' + style + '})')
+      this.$element.css( styles )
     }
-  };
+  }
 
   function Plugin(option) {
     return this.each(function () {
-      var $this   = $(this);
-      var data    = $this.data('changeonscroll');
-      var options = $.extend({}, ChangeOnScroll.DEFAULTS, data, option);
-      new ChangeOnScroll($this, options);
-    });
+      var $this   = $(this)
+      var data    = $this.data('changeonscroll')
+      var options = $.extend({}, ChangeOnScroll.DEFAULTS, data, option)
+      new ChangeOnScroll($this, options)
+    })
   }
 
-  var old = $.fn.changeonscroll;
+  var old = $.fn.changeonscroll
 
-  $.fn.changeonscroll             = Plugin;
-  $.fn.changeonscroll.Constructor = ChangeOnScroll;
+  $.fn.changeonscroll             = Plugin
+  $.fn.changeonscroll.Constructor = ChangeOnScroll
 
   $.fn.changeonscroll.noConflict = function () {
-    $.fn.changeonscroll = old;
-    return this;
-  };
+    $.fn.changeonscroll = old
+    return this
+  }
 
   $(window).on('load', function(){
+    $('[data-spy="scroller"]').each(function(){
+      var $spy = $(this)
+      var data = $spy.data()
+      Plugin.call($spy, data)
+    })
     $(this).on('scroll.changeonscroll.data-api', function() {
       $('[data-spy="scroller"]').each(function(){
-        var $spy = $(this);
-        var data = $spy.data();
-        Plugin.call($spy, data);
-      });
-    });
-  });
+        var $spy = $(this)
+        var data = $spy.data()
+        Plugin.call($spy, data)
+      })
+    })
+  })
 
 }(jQuery);
