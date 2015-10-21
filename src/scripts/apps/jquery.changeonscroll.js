8d0e99b1f40270d6f349dab6e2e89bbf3f294479
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-10-22 00:43:05 +0200

    add parent offset

--- src/scripts/apps/jquery.changeonscroll.js
+++ src/scripts/apps/jquery.changeonscroll.js
@@ -14,6 +14,7 @@
     this.$element       = $(element)
     this.data           = this.$element.data()
     this.options        = $.extend({}, ChangeOnScroll.DEFAULTS, this.data, options)
+    this.$parent         = typeof this.options.parent === 'boolean' ? this.$element.parent() : $(this.options.parent)
 
     this.pos            = $(window).scrollTop()
     this.top            = this.offsetTop()
@@ -38,8 +39,8 @@
     whileClass        : 'scroller_while',
     afterClass        : 'scroller_after',
     unit              : '',
-    top               : 0,
-    bottom            : 0,
+    top               : '',
+    bottom            : '',
     start             : '',
     end               : '',
     factor            : 1
@@ -91,23 +92,26 @@
   }
 
   ChangeOnScroll.prototype.offsetTop = function() {
-    var trigger = this.options.top
+    var element = this.options.parent ? this.$parent : this.$element
+    var trigger = this.options.top ? this.options.top : element
     var offsetTop
     if(trigger){
       offsetTop = typeof trigger === 'number' ? trigger :  $(trigger).offset().top
     } else {
-      offsetTop = typeof this.options.top === 'number' ? this.options.top : this.$element.offset().top
+      offsetTop = typeof this.options.top === 'number' ? this.options.top : element.offset().top
     }
     return offsetTop;
   }
 
   ChangeOnScroll.prototype.offsetBottom = function() {
-    var trigger = this.options.bottom
+    var element = this.options.parent ? this.$parent : this.$element
+    var trigger = this.options.bottom ? this.options.bottom : element
     var offsetBottom
+    var elementHeight = element.outerHeight()
     if(trigger){
-      offsetBottom = typeof trigger === 'number' ? trigger : $(trigger).offset().top
+      offsetBottom = typeof trigger === 'number' ? trigger : $(trigger).offset().top + elementHeight
     } else {
-      offsetBottom = typeof this.options.bottom === 'number' ? this.options.bottom : this.$element.offset().top
+      offsetBottom = typeof this.options.bottom === 'number' ? this.options.bottom : element.offset().top + elementHeight
     }
     return offsetBottom;
   }
